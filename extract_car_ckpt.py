7479efbe4fa30fff65b10e95c1b677a3e690473b
Ziyu Yao <123848076+MiracleDance@users.noreply.github.com>
2024-11-22 13:15:34 +0800

    Delete extract_car_ckpt.py

--- extract_car_ckpt.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-best_ckpt = '/data/yzy/projects/conVAR_residual/local_output_car_hed_lr0.001/ar-ckpt-best.pth'
-a = torch.load(best_ckpt, map_location='cpu')['trainer']['car_wo_ddp']
-b = {}
-for k, v in a.items():
-    if 'car' in k:
-        b[k] = v
-torch.save(b, '/data/yzy/projects/conVAR_residual/local_output_car_hed_lr0.001/ckpt-best.pth')
